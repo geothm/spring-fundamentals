ecb5196ebdc21add5a65ad0f7697f283cbe04a3e
gpapp <georgia.papp@irian.ro>
2024-07-15 19:51:26 +0300

    S 3.1 Servlet implementation

--- src/main/java/ro/Main.java
+++ src/main/java/ro/Main.java
@@ -2,8 +2,10 @@ package ro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ src/main/java/ro/servlet/MyServlet.java
@@ -0,0 +1,31 @@
+package ro.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/myservlet")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+// Initialization code, e.g., setting up resources
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body></html>");
+    }
+    @Override
+    public void destroy() {
+// Cleanup code, e.g., releasing resources
+    }
+}
